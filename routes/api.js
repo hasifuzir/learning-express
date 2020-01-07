501c52c21d4d01e68f988d6b417a4c5b0fe510f4
hasifuzir <hasifuzir@gmail.com>
2020-01-07 10:24:18 +0800

    Created a CRUD api for user wishlist at /wishlist

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 //const _ = require('lodash');
 const createError = require('http-errors');
-const apiResponse = require('../helpers/errorResponse');
+const errorResponse = require('../helpers/errorResponse');
 const schemas = require('../helpers/schemas');
 
 //Middleware
@@ -19,12 +19,12 @@ router.get('/all', function(req, res) {
     res.status(200);
 
 
-    return res.json(apiResponse.success(games.items));
+    return res.json(errorResponse.success(games.items));
   }
   catch(err) {
     res.status(400);
 
-    return res.json(apiResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
   }
 });
 
@@ -35,7 +35,7 @@ router.get('/id_list', function(req, res, next) {
     let gameIds = games.items.map(game => game.id);
     res.status(200);
 
-    return res.json(apiResponse.success(gameIds));
+    return res.json(errorResponse.success(gameIds));
   }
   catch(err){
     res.status(400);
@@ -53,17 +53,17 @@ router.get('/game/:id', validator(schemas.gameSchema), (req, res, next) => {
     if (game !== undefined) {
       res.status(200);
 
-      return res.json(apiResponse.success(game));
+      return res.json(errorResponse.success(game));
     }
     else {
       res.status(400);
-      return res.json(apiResponse.fail(400,'BAD_REQUEST', 'NOT_FOUND', 'Invalid game ID!'));
+      return res.json(errorResponse.fail(400,'BAD_REQUEST', 'NOT_FOUND', 'Invalid game ID!'));
     }
   }
   catch(err) {
     res.status(400);
 
-    return res.json(apiResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
   }
 });
 
@@ -76,18 +76,18 @@ router.get('/platform/:id', validator(schemas.gameSchema), function(req, res, ne
     if (gameList && gameList.length) {
       res.status(200);
 
-      return res.json(apiResponse.success(gameList));
+      return res.json(errorResponse.success(gameList));
     }
     else {
       res.status(400);
 
-      return res.json(apiResponse.fail(400,'BAD_REQUEST', 'NOT_FOUND', 'Invalid platform ID!'));
+      return res.json(errorResponse.fail(400,'BAD_REQUEST', 'NOT_FOUND', 'Invalid platform ID!'));
     }
   }
   catch(err) {
     res.status(400);
 
-    return res.json(apiResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
   }
 });
 
@@ -96,14 +96,121 @@ router.get('/profile', function(req, res, next) {
   try {
     const profile = JSON.parse(fs.readFileSync('./public/json/profile.json'));
 
-    return res.json(apiResponse.success(profile));
+    return res.json(errorResponse.success(profile));
   }
   catch(err) {
     res.status(400);
 
-    return res.json(apiResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
   }
 });
+
+//Return profile details as JSON
+router.get('/profile/wishlist', function(req, res, next) {
+  try {
+    const profile = JSON.parse(fs.readFileSync('./public/json/profile.json'));
+
+    return res.json(errorResponse.success(profile.wishlist));
+  }
+  catch(err) {
+    res.status(400);
+
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'UNKNOWN_ERROR', 'API has returned an error!'));
+  }
+});
+
+//Add a game to the user profile's wishlist
+//wishName
+router.post('/profile/wishlist', validator(schemas.wishListSchema), (req, res, next) => {
+  try {
+    const profile = JSON.parse(fs.readFileSync('./public/json/profile.json'));
+
+    let newWish = req.body.wishName;
+
+    profile.wishlist.forEach(id => {
+      if (id.toLowerCase() === newWish.toLowerCase()){
+        throw new Error('Wishlist game already exists!');
+      }
+    });
+
+    profile.wishlist.push(newWish);
+
+    let newProfile = JSON.stringify(profile);
+    fs.writeFile('./public/json/profile.json', newProfile, 'utf8', function (err) {
+      if (err) {
+        throw err;
+      } else {
+        return res.json(errorResponse.success('New game successfully added to wishlist'));
+      }
+    });
+  }
+  catch(err) {
+    res.status(400);
+
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'DUPLICATE_ERROR', err.message));
+  }
+});
+
+//Edit a game on the wishlist
+//wishName
+router.put('/profile/wishlist/:index', validator(schemas.indexSchema), (req, res, next) => {
+  try {
+    const profile = JSON.parse(fs.readFileSync('./public/json/profile.json'));
+    const wishIndex = req.params.index;
+
+    if (typeof profile.wishlist[wishIndex] === 'undefined'){
+      throw  Error('Wishlist game does not exist!');
+    }
+    else {
+      profile.wishlist[wishIndex] = req.body.wishName;
+    }
+
+    let newProfile = JSON.stringify(profile);
+    fs.writeFile('./public/json/profile.json', newProfile, 'utf8', function (err) {
+      if (err) {
+        throw err;
+      } else {
+        return res.json(errorResponse.success('Wishlist game name successfully updated'));
+      }
+    });
+  }
+  catch(err) {
+    res.status(400);
+
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'INDEX_ERROR', err.message));
+  }
+});
+
+//Delete a game on the wishlist
+//wishName
+router.delete('/profile/wishlist/:index', validator(schemas.indexSchema), (req, res, next) => {
+  try {
+    const profile = JSON.parse(fs.readFileSync('./public/json/profile.json'));
+    const wishIndex = req.params.index;
+
+    if (typeof profile.wishlist[wishIndex] === 'undefined'){
+      throw  Error('Wishlist game does not exist!');
+    }
+    else {
+      profile.wishlist.splice(wishIndex, 1);
+    }
+
+    let newProfile = JSON.stringify(profile);
+    fs.writeFile('./public/json/profile.json', newProfile, 'utf8', function (err) {
+      if (err) {
+        throw err;
+      } else {
+        return res.json(errorResponse.success('Wishlist game successfully deleted'));
+      }
+    });
+  }
+  catch(err) {
+    res.status(400);
+
+    return res.json(errorResponse.fail(400, 'BAD_REQUEST', 'INDEX_ERROR', err.message));
+  }
+});
+
 
 //Return all games based on specified platform ID as JSON
 //Uses lodash, but you can also use filter() + inludes() instead!
@@ -114,6 +221,8 @@ router.get('/platform/:id', function(req, res) {
   return res.json(gameList);
 });
  */
+
+
 
 
 module.exports = router;
